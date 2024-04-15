69556c03a3609b6bcf7d1785f48bbbb71ee3c8a9
noazlee <noahzlee5@gmail.com>
2024-04-15 12:29:15 -0500

    edited docker files and added / route

--- server/app.js
+++ server/app.js
@@ -8,11 +8,13 @@ const io = socketIo(server);
 
 const pokerLogic = require('./routes/poker-logic.js');
 
-io.on('connection', (socket) => {
-  console.log('New client connected:', socket.id);
-  // Implement poker game logic here
-  socket.on('disconnect', () => console.log('Client disconnected'));
-});
+// io.on('connection', (socket) => {
+//   console.log('New client connected:', socket.id);
+//   // Implement poker game logic here
+//   socket.on('disconnect', () => console.log('Client disconnected'));
+// });
+
+app.use(pokerLogic);
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => console.log(`Listening on port ${PORT}`));
--- server/routes/poker-logic.js
+++ server/routes/poker-logic.js
@@ -7,6 +7,10 @@ router.get('/add-product',(req,res,next)=>{
     res.send('<form action="/admin/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
 }); 
 
+router.get('/',(req,res,next)=>{
+    res.write('<h1>Hello world</h1>');
+}); 
+
 router.post('/product',(req,res,next)=>{
     console.log(req.body.title);
     res.redirect('/');
