204f4e674f91555dad4a90adb8b2fe9b2929ee0f
noazlee <noahzlee5@gmail.com>
2024-04-15 12:29:36 -0500

    edited docker files and added / route

--- server/routes/poker-logic.js
+++ server/routes/poker-logic.js
@@ -7,13 +7,17 @@ router.get('/add-product',(req,res,next)=>{
     res.send('<form action="/admin/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
 }); 
 
-router.get('/',(req,res,next)=>{
-    res.write('<h1>Hello world</h1>');
-}); 
-
 router.post('/product',(req,res,next)=>{
     console.log(req.body.title);
     res.redirect('/');
 }); 
+
+//tests
+
+router.get('/',(req,res,next)=>{
+    res.write('<h1>Hello world</h1>');
+}); 
+
+
 
 module.exports = router;
