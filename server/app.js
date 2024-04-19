9fb9ffb0b8145048c51ba36dd6a602c787261447
noazlee <noahzlee5@gmail.com>
2024-04-18 23:12:24 -0500

    changes

--- server/app.js
+++ server/app.js
@@ -12,11 +12,21 @@ const game = require('./routes/game.js');
 io.on('connection', (socket) => {
   console.log('New client connected:', socket.id);
   // Implement poker game logic here
+  
+  socket.on('create-game', (data) => {
+    // Add user to a game room, for example
+    socket.join(data.gameId);
+    // Notify others in the room
+    // socket.to(data.gameId).emit('player-joined', { playerId: socket.id, gameId: data.gameId });
+    console.log(`User ${socket.id} created a game`);
+  });
+  
   socket.on('join-game', (data) => {
     // Add user to a game room, for example
     socket.join(data.gameId);
     // Notify others in the room
-    socket.to(data.gameId).emit('player-joined', { playerId: socket.id, gameId: data.gameId });
+    // socket.to(data.gameId).emit('player-joined', { playerId: socket.id, gameId: data.gameId });
+    console.log(`User ${socket.id} joined a game`);
   });
   socket.on('disconnect', () => console.log('Client disconnected'));
 });
--- server/routes/poker-logic.js
+++ server/routes/poker-logic.js
@@ -3,18 +3,18 @@ const express = require('express');
 const router = express.Router();
 
 // EXAMPLES
-router.get('/add-product',(req,res,next)=>{
-    res.send('<form action="/admin/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
-}); 
+// router.get('/add-product',(req,res,next)=>{
+//     res.send('<form action="/admin/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
+// }); 
 
-router.post('/product',(req,res,next)=>{
-    console.log(req.body.title);
-    res.redirect('/');
-}); 
+// router.post('/product',(req,res,next)=>{
+//     console.log(req.body.title);
+//     res.redirect('/');
+// }); 
 
 //tests
 router.get('/',(req,res,next)=>{
-    res.write('<h1>Hello world</h1>');
+    res.write('<h1>Hello world2</h1>');
 }); 
 
 
