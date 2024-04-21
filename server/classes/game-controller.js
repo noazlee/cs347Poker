5ad3a0724a2c28e54b6e0124d209068e68a130ac
noazlee <noahzlee5@gmail.com>
2024-04-21 13:16:16 -0500

    deleted game-controller

--- server/classes/game-controller.js
+++
@@ -1,9 +0,0 @@
-const Game = require('./game-model');
-const Table = require('./table');
-const Player = require('./player');
-
-class GameController {
-    constructor() {
-        this.games = {}; //stores all games
-    }
-}
