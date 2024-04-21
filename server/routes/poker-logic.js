a1064adb8e05be1d39336d086d7a5af4e8103cb7
noazlee <noahzlee5@gmail.com>
2024-04-21 14:33:45 -0500

    changed poker logic

--- server/routes/poker-logic.js
+++ server/routes/poker-logic.js
@@ -1,6 +1,12 @@
 const express = require('express');
+const Table = require('../classes/table');
+const Player = require('../classes/player');
+
+
+
 
 const router = express.Router();
+const games = require('./game-logic');
 
 // EXAMPLES
 // router.get('/add-product',(req,res,next)=>{
@@ -17,29 +23,29 @@ router.get('/',(req,res,next)=>{
     res.write('<h1>Hello world2</h1>');
 }); 
 
-router.post('/start-game/:gameId', (req, res) => {
+router.post('/start-game/:gameId/', (req, res, next) => {
     const { gameId } = req.params;
     // table = New Table();
     // res.json({table});
+    table[table_id] = {
+
+    }
 });
 
-router.post('/table/init', (req, res) => {
-    const { num_players, starting_chips } = req.body;
-
-    // Create a new table ID
-    const table_id = Object.keys(tables).length + 1;
-
-    // Initialize the table with the specified number of players and starting chips
-    tables[table_id] = {
-        players: Array.from({ length: num_players }, (_, i) => ({
-            id: i + 1,
-            chips: starting_chips
-        })),
-        pot: 0
-    };
-
-    res.json({ success: true, message: 'Table initialized successfully', table_id });
+router.post('/round-of-betting/:gameId/:stage', (req, re, next) => {
+    // go through queue, send request to client when it is their turn
 });
+
+router.post('/show-cards/:gameId/:stage', (req, res, next) => {
+    // if stage == 0 => flop
+    // if stage == 1 => turn
+    // if stage == 2 => river
+    // if stage == 3 => calculate winner () - cycle through each player in queue, take their hand and combine with community cards
+    // to calculate the best combination of 5 cards and assign a 'strength value 1-10'
+    // compare each players strength value, add cases when they are equal
+});
+
+
 
 
 module.exports = router;
