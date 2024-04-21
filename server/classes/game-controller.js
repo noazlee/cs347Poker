18eea7cdb0997b5dc15c4697d1727d32c7e2e528
noazlee <noahzlee5@gmail.com>
2024-04-21 13:15:36 -0500

    added game-controller

---
+++ server/classes/game-controller.js
@@ -0,0 +1,9 @@
+const Game = require('./game-model');
+const Table = require('./table');
+const Player = require('./player');
+
+class GameController {
+    constructor() {
+        this.games = {}; //stores all games
+    }
+}
