e69f309205428f4623cef8b3c4932b9937276e9c
ashokhare <ashokhare@LAPTOP-T4GIOLUF>
2024-04-30 20:39:17 -0500

    adding raise popup

---
+++ client/src/components/Popup.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import '../css/Popup.css';
+
+export default function Popup ({ isDisplayed, togglePopup, props }) {
+    const [raiseAmount, changeRaiseAmount] = useState(0)
+
+    const goAllIn = () => {
+        changeRaiseAmount(50);
+    }
+
+    const handleSubmit = () => {
+        console.log(document.getElementById('raiseAmount').value);
+        togglePopup(false);
+    }
+
+    return isDisplayed === true ? (
+        <div className='popup'>
+            <div id='raiseForm'>
+                <h2>Select Amount to Raise</h2>
+                <input type='number' name='raiseAmount' id='raiseAmount' value={raiseAmount} onChange={(e) => changeRaiseAmount(e.target.value)}/>
+                <button onClick={goAllIn}>Go All In!</button>
+                <button onClick={() => togglePopup(false)}>Cancel</button>
+                <button onClick={handleSubmit}>Confirm</button>
+            </div>
+        </div>
+    ) : (
+        null
+    )
+}
