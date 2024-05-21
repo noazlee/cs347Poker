4fe6442db6260ac5e17fb539e9b23adef8dc343e
noazlee <noahzlee5@gmail.com>
2024-05-20 22:47:32 -0500

    fixed date in game

--- server/classes/game-model.js
+++ server/classes/game-model.js
@@ -6,15 +6,16 @@ const POTAMOUNT = 10000;
 const GAMEMODE = 'regular';
 const SMALLBLINDAMOUNT = 200;
 
-const format24Hour = (
-    {
-        dd, mm, yyyy,
-        HH, MM, SS
-    }) => {
-    date = `${mm}/${dd}/${yyyy} ${HH}:${MM}:${SS}`;
-    console.log(date);
-    return date;
-    
+const format24Hour = (date) => {
+    const dd = String(date.getDate()).padStart(2, '0');
+    const mm = String(date.getMonth() + 1).padStart(2, '0'); 
+    const yyyy = date.getFullYear();
+    const HH = String(date.getHours()).padStart(2, '0');
+    const MM = String(date.getMinutes()).padStart(2, '0');
+    const SS = String(date.getSeconds()).padStart(2, '0');
+
+    const newDate = `${mm}/${dd}/${yyyy} ${HH}:${MM}:${SS}`;
+    return newDate;
 };
 
 class Game {
