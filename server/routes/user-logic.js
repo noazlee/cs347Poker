e260a404bba90fd71aa2626b4dbfe0afe41851fb
noazlee <noahzlee5@gmail.com>
2024-05-28 15:45:15 -0500

    game history is working

--- server/routes/user-logic.js
+++ server/routes/user-logic.js
@@ -128,7 +128,6 @@ router.get('/games/:userId', async (req, res) => {
         const db = await connectDb();
 
         const games = await db.collection('games').find({ "players.userId": userId }).toArray();
-
         games.forEach(game => {
             if (game.rounds) {
                 game.rounds.forEach(round => {
@@ -153,26 +152,24 @@ router.get('/games/:userId/:gameId', async (req, res) => {
     const { userId, gameId } = req.params;
     try {
         const db = await connectDb();
-
         const games = await db.collection('games').find({ "players.userId": userId }).toArray();
 
-        games.forEach(game => {
-            if (game.rounds) {
-                game.players.forEach(player=>{
-                    if(player.userId===userId && game.gameId === gameId){
-                        game.rounds.forEach(round => {
-                            delete round.deck;
-                        });
-                        return res.status(200).json(game);
-                    }
-                })  
+        let gameFound = false;
+        games.some(game => {
+            if (game.gameId === gameId) {
+                if (game.rounds) {
+                    game.rounds.forEach(round => {
+                        delete round.deck;
+                    });
+                }
+                gameFound = true;
+                return res.status(200).json(game);
             }
+            return false;
         });
 
-        if (games) {
-            return res.status(200).json(games);
-        } else {
-            return res.status(404).json({ message: 'Game not found', game:games });
+        if (!gameFound) {
+            return res.status(404).json({ message: 'Game not found' });
         }
     } catch (error) {
         console.error('Failed to retrieve game:', error);
