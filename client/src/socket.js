3b58ca03db88de28381d032fabdb5770ddf95ea2
Noah Lee <156212285+noazlee@users.noreply.github.com>
2024-07-25 22:01:33 +0800

    Update socket.js

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,6 +1,6 @@
 import io from 'socket.io-client';
 
-const acceptedURL = window.location.hostname === 'localhost' ? 'http://localhost:3000' : 'http://10.133.26.36:3000';
+const acceptedURL = window.location.hostname === 'localhost' ? 'http://localhost:3000' : 'IP ADDRESS';
 const socket = io(acceptedURL, { withCredentials: true, transports: ['websocket', 'polling'] });
 
 export default socket;
